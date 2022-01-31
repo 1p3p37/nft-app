998df403a601a40a775225fe86d3924f18d6140c
1p3p37 <nejdan4ik86@gmail.com>
2022-02-01 00:37:39 +0300

    create_token|35/100|

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+from typing import List
+
 from fastapi import FastAPI, HTTPException
 from models.models import token_pydantic, tokenIn_pydantic, Tokens
 from pydantic import BaseModel
+
+# generate rand string
+import random
+import string
+
 app = FastAPI()
 
 
-@app.post("/tokens/create")
-async def create_token(name: str):
-    name_token = name
-    return {"token": f"{name_token}"}
+def random_string():
+    letters = string.ascii_letters + string.digits
+    rand_string = ''.join(random.choice(letters) for i in range(20))
+    return rand_string
 
 
-@app.get("/tokens/list")
+@app.post("/tokens/create", response_model=token_pydantic)
+async def create_token(token: token_pydantic):
+    unique_hash = random_string()
+    #media_url =
+    #owner =
+    token_obj = await Tokens.create(**token.dict(exclude_unset=True))
+    return await token_pydantic.from_tortoise_orm(token_obj)
+
+
+@app.get("/tokens/list", response_model=List[token_pydantic])
 async def list_tokens():
-    return {"list"}
+    return await token_pydantic.from_queryset(Tokens.all())
 
 
 @app.get("/tokens/total_supply")
-async def list_tokens():
+async def total_supply_token():
     return {"result": 10000}
